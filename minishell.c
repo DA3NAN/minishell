9c5044f2d93d43ebc0fc75d8cc46eaf7281ac7b8
Adnane Ait Malek <aait-mal@e2r1p3.1337.ma>
2023-06-07 17:32:01 +0100

    prompt

--- minishell.c
+++ minishell.c
@@ -6,21 +6,37 @@
 /*   By: aait-mal <aait-mal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 15:18:45 by aait-mal          #+#    #+#             */
-/*   Updated: 2023/06/07 15:29:36 by aait-mal         ###   ########.fr       */
+/*   Updated: 2023/06/07 17:31:05 by aait-mal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(void)
+static void	sigusr_handler(int signum)
+{
+	if (signum == SIGINT)
+	{
+		write(1, "\nminishell# ", 12);
+	}
+}
+
+void	display_prompt(void)
 {
 	char	*input;
 
 	input = readline("minishell# ");
-	if (input)
+	if (!input)
+		exit (0);
+	printf("You entered : %s\n", input);
+	free(input);
+}
+
+int	main(void)
+{
+	signal(SIGINT, sigusr_handler);
+	while (1)
 	{
-		printf("You entered : %s\n", input);
-		free(input);
+		display_prompt();
 	}
 	return (0);
 }
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: aait-mal <aait-mal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 15:15:43 by aait-mal          #+#    #+#             */
-/*   Updated: 2023/06/07 15:22:43 by aait-mal         ###   ########.fr       */
+/*   Updated: 2023/06/07 16:51:40 by aait-mal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <signal.h>
 # include <readline/readline.h>
+
+void	display_prompt(void);
 
 #endif
